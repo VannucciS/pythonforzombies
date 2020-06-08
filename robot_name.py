2ebec909ef7ef6b8b4f1f512ac6f1a550c354eaa
VannucciS <50140951+VannucciS@users.noreply.github.com>
2020-06-08 00:22:35 -0300

    Update robot_name.py

--- robot_name.py
+++ robot_name.py
@@ -1,8 +1,18 @@
 # create a robot name like RX837 or BC811
 # Two uppercase letter followed by 3 numbers
 
-from string import ascii_lowercase
+import string
 import random
-names = []
-  def robotName():
+
+def robotName():
+    names = []
+    letters = string.ascii_uppercase
+    numbers = string.digits
+    num = "".join(random.choice(letters) for i in range (2))
+    let = "".join(random.choice(numbers) for i in range (3))
+    name = num + let
+    if name not in name:
+        names.append(name)
+        return name
+    else:
   
