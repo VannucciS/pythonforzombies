1a9a4cffa2761d895a276d5add32ec12ef72abe4
VannucciS <50140951+VannucciS@users.noreply.github.com>
2020-06-08 00:31:26 -0300

    Update robot_name.py

--- robot_name.py
+++ robot_name.py
@@ -8,11 +8,12 @@ def robotName():
     names = []
     letters = string.ascii_uppercase
     numbers = string.digits
-    num = "".join(random.choice(letters) for i in range (2))
-    let = "".join(random.choice(numbers) for i in range (3))
-    name = num + let
-    if name not in name:
-        names.append(name)
-        return name
-    else:
-  
+    
+    while true:
+        num = "".join(random.choice(letters) for i in range (2))
+        let = "".join(random.choice(numbers) for i in range (3))
+        name = num + let
+        if name not in names:
+            names.append(name)
+            break    
+   return name
